eda7497bd91a7e009930f241ba6b29a80f362442
slava-pc\Slava <slavasotnikov2020@ukr.net>
2022-10-30 20:14:15 +0200

    Table3.1(3)

---
+++ Table3.1(3)/Table3.1(3)/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Table3._1_3_
+{
+    internal class Program
+    {
+        /// <summary>
+        /// Для масиву, що відсортований за зростанням, вивести номер елемента, 
+        /// після якого потрібно вставити деяке число y.
+        /// </summary>
+
+        static void Main()
+        {
+            int[] array = { 23,8,16,50,27,61,2 };
+            int insert = 20;
+            int i;
+
+            Array.Sort(array);
+
+            for (i = 0; i < array.Length - 1; i++)
+            {
+                if (insert > array[i] && insert < array[i + 1])
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine("Insert after: {0}", i);
+            Console.ReadKey();
+        }
+    }
+}
